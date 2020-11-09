01d9aafcc9d8868b0445e297482ddaef93d7d70b
Benjamin Legrand <benjilegnard@protonmail.com>
2020-11-09 17:25:38 +0100

    fix: add hidden title, fix test, ignore .cache

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hidden title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Kaossilator/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Kaossillator from './app/components/Kaossillator';
 
 if (typeof AudioContext === "undefined")
-  alert('audio api not supported');
+  console.error('audio api not supported');
 
 export default function App()
 {
-    return (<Kaossillator></Kaossillator>)
+    return (<div><h1 className="sr-only">Kaossilator</h1><Kaossillator></Kaossillator></div>)
 }
