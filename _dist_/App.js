88dfff2d8591c808b80c4f43bef4a25ce320e909
Benjamin Legrand <jibhaine@free.fr>
2020-11-09 16:26:54 +0000

    Updates

--- _dist_/App.js
+++ _dist_/App.js
@@ -1,7 +1,9 @@
 import React from '../web_modules/react.js';
 import Kaossillator from './app/components/Kaossillator.js';
-if (typeof AudioContext === "undefined") alert('audio api not supported');
+if (typeof AudioContext === "undefined") console.error('audio api not supported');
 export default function App() {
-  return /*#__PURE__*/React.createElement(Kaossillator, null);
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
+    className: "sr-only"
+  }, "Kaossilator"), /*#__PURE__*/React.createElement(Kaossillator, null));
 }
 //# sourceMappingURL=App.js.map
